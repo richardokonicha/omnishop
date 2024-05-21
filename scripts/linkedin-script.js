535c4b6db8fe1c73c8ad7a3908c2e4dfc73a5b79
riri <richardokonicha@gmail.com>
2024-05-22 00:38:57 +0100

    operational getting chats and header/ wip

--- scripts/linkedin-script.js
+++ scripts/linkedin-script.js
@@ -46,67 +46,105 @@ const contextOptions = {
     const page = await context.newPage();
     await page.goto(PEOPLE_URL);
 
-    // from this line should be a standalone function that can be called from recursively after a next button click
-    await page.waitForSelector('.linked-area', { timeout: 10000 });
-
-    // Get all elements with the class 'linked-area'
-    const linkedAreaElements = await page.locator('.linked-area');
-    const count = await linkedAreaElements.count();
-    console.log(`Found ${count} linked-area elements`);
-
-    let messageButtons = [];
-
-    for (let i = 0; i < count; i++) {
-        const linkedAreaElement = linkedAreaElements.nth(i);
-        const messageButton = await linkedAreaElement.locator('button:has-text("Message")').elementHandle();
-        if (messageButton) {
-            messageButtons.push(messageButton);
+    async function handleClose(page) {
+        if (await page.getByRole('button', { name: 'Close your conversation with' }).isVisible()) {
+            await page.getByRole('button', { name: 'Close your conversation with' }).click();
+            await page.waitForTimeout(500);
+        }
+        if (await page.getByRole('button', { name: 'Close your draft conversation' }).isVisible()) {
+            await page.getByRole('button', { name: 'Close your draft conversation' }).click();
+            await page.waitForTimeout(500);
+        }
+        if (!await page.getByRole('button', { name: 'Close your draft conversation' }).isVisible() && !await page.getByRole('button', { name: 'Close your conversation with' }).isVisible()){
+            console.log("no close found")
         }
     }
 
-    console.log(`Found ${messageButtons.length} message buttons`);
 
-    for (const messageButton of messageButtons) {
-        try {
+    async function handleMButton(btn) {
+        await btn.scrollIntoViewIfNeeded();
+        await page.waitForTimeout(500);
+        await btn.hover();
+        await btn.click({ force: true });
+        console.log('Message button clicked');
+        await page.waitForTimeout(1000);
+    }
+
+    async function handleMessage(page) {
+        let message = 'Hello, I am a recruiter at Omni. I would like to talk to you about a job opportunity. Please let me know if you are interested.';
+        await page.waitForTimeout(500);
+        let msgFrame = await page.getByLabel('Messaging')
+        if (!await msgFrame.isVisible()){
+            console.log('msgFrame is not visible')
+            return
+        }
+
+        await page.waitForTimeout(100);
+        await msgFrame.scrollIntoViewIfNeeded();
+        await msgFrame.hover();
+        await msgFrame.click();
+
+        const conversationText = await msgFrame.locator("//p").allTextContents();
+        const headerText = await msgFrame.locator("//header//a").innerText();
+
+        await page.waitForTimeout(100);
+
+        console.log('headerText:', headerText);
+
+        if (await page.getByRole('button', { name: 'Send' }).isVisible()) {
+            await page.fill('textarea', message);
+            // await page.getByRole('button', { name: 'Send' }).click();
+            console.log('Message sent');
+            await page.waitForTimeout(500);
+        } else {
+            console.log('Send button is not visible');
+            handleClose(page)
+        }
+
+    }
+
+    async function processMessageButtons(page) {
+        await page.waitForSelector('.linked-area', { timeout: 10000 });
+        const linkedAreaElements = await page.locator('.linked-area');
+        const count = await linkedAreaElements.count();
+        console.log(`Found ${count} linked-area elements`);
+        let messageButtons = [];
+
+        for (let i = 0; i < count; i++) {
+            const linkedAreaElement = linkedAreaElements.nth(i);
+            const messageButton = await linkedAreaElement.locator('button:has-text("Message")').elementHandle();
             if (messageButton) {
-
-                if (await page.getByRole('button', { name: 'Close your conversation with' }).isVisible()) {
-                    await page.getByRole('button', { name: 'Close your conversation with' }).click();
-                    await page.waitForTimeout(500);
-                } else if (await page.getByRole('button', { name: 'Close your draft conversation' }).isVisible()) {
-                    await page.getByRole('button', { name: 'Close your draft conversation' }).click();
-                    await page.waitForTimeout(500);
-                }
-
-                // Scroll to the message button before attempting to hover and click
-                await messageButton.scrollIntoViewIfNeeded();
-                await page.waitForTimeout(500); // Wait for the scroll action to complete
-                await messageButton.hover();
-                await messageButton.click({ force: true });
-                console.log('Message button clicked');
-
-                await page.waitForTimeout(1000)
-
-                // Check for the close buttons again after opening the chat
-                if (await page.getByRole('button', { name: 'Close your conversation with' }).isVisible()) {
-                    await page.getByRole('button', { name: 'Close your conversation with' }).click();
-                    console.log('Chat closed');
-                } else if (await page.getByRole('button', { name: 'Close your draft conversation' }).isVisible()) {
-                    await page.getByRole('button', { name: 'Close your draft conversation' }).click();
-                    console.log('Chat closed');
-                } else {
-                    console.log('Close button not found');
-                }
-            } else {
-                console.log('Message button is not an element');
+                messageButtons.push(messageButton);
             }
-        } catch (error) {
-            console.error(`Error interacting with message button: ${error}`);
+        }
+
+        console.log(`Found ${messageButtons.length} message buttons`);
+
+        for (const messageButton of messageButtons) {
+            try {
+                if (messageButton) {
+                    await handleClose(page)
+                    await handleMButton(messageButton)
+                    await handleMessage(page)
+                    await handleClose(page)
+                } else {
+                    console.log('Message button is not an element');
+                }
+            } catch (error) {
+                console.error(`Error interacting with message button: ${error}`);
+            }
+        }
+
+        await page.waitForTimeout(2000);
+
+        if (await page.getByLabel('Next').isVisible()) {
+            await page.getByLabel('Next').click();
+            await page.waitForTimeout(1000);
+            await processMessageButtons(page);
+        } else {
+            console.log('No more next button found');
         }
     }
-    await page.waitForTimeout(1000); // Wait for the scroll action to complete
-    await page.getByLabel('Next').click();
-    await page.waitForTimeout(1000); // Wait for the scroll action to complete
-
+    await processMessageButtons(page);
     await context.close();
 })();
